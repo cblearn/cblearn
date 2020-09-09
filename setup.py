2e207c1edd571de92737b535d6f59eff96c0c196
David-Elias Kuenstle <david-elias.kuenstle@uni-tuebingen.de>
2020-09-09 16:30:30 +0200

    Add basic package structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+""" Dynamic configuration of distribution package. """
+
+import os
+
+from setuptools import find_packages, setup
+
+# get __version__ from _version.py
+ver_file = os.path.join('ordcomp', '_version.py')
+with open(ver_file) as f:
+    exec(f.read())
+
+setup(version=__version__)
