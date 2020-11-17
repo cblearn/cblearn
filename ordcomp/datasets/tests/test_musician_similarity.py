a886a47f01a3b317c58fd4c1765e73a9678fdcf3
David-Elias Kuenstle <david-elias.kuenstle@uni-tuebingen.de>
2020-11-17 11:15:49 +0100

    Dataset tests download everytime to a temporal directory.

--- ordcomp/datasets/tests/test_musician_similarity.py
+++ ordcomp/datasets/tests/test_musician_similarity.py
@@ -3,8 +3,9 @@ import numpy as np
 from ordcomp.datasets import fetch_musician_similarity
 
 
-def test_fetch_musician_similarity():
-    bunch = fetch_musician_similarity(shuffle=False)
+def test_fetch_musician_similarity(tmp_path):
+    data_home = tmp_path / 'ordcomp_datasets'
+    bunch = fetch_musician_similarity(data_home=data_home, shuffle=False)
 
     assert bunch.data.shape == (213629, 3)
     assert bunch.judgement_id.shape == (213629, )
@@ -13,10 +14,10 @@ def test_fetch_musician_similarity():
     assert bunch.artists.shape == (413, )
     assert bunch.artists[bunch.data[0, 0]] == 'queen'
 
-    triplets = fetch_musician_similarity(shuffle=False, return_triplets=True)
+    triplets = fetch_musician_similarity(data_home=data_home, shuffle=False, return_triplets=True)
     np.testing.assert_equal(bunch.data, triplets)
 
-    shuffle_bunch = fetch_musician_similarity(random_state=42)
+    shuffle_bunch = fetch_musician_similarity(data_home=data_home, random_state=42)
     assert not np.all(shuffle_bunch.data == bunch.data)
     assert not np.all(shuffle_bunch.user == bunch.user)
     np.testing.assert_equal(shuffle_bunch.user.sort(), bunch.user.sort())
