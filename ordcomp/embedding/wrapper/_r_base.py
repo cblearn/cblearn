b63d3bc78da97fcfa9461c8734bb3c9f7b818c34
David-Elias Kuenstle <david-elias.kuenstle@uni-tuebingen.de>
2020-11-17 11:22:29 +0100

    Add utility class to access R libraries

--- ordcomp/embedding/wrapper/_r_base.py
+++ ordcomp/embedding/wrapper/_r_base.py
@@ -1,46 +1,43 @@
-
-
-
-class RWrapper:
+class RWrapperMixin:
     imported_packages = {}
 
-    def _init_r(self):
+    @classmethod
+    def _init_r(cls):
         try:
             from rpy2 import robjects
             from rpy2.robjects import numpy2ri
             from rpy2.robjects import packages
             from rpy2.robjects import vectors
 
-            self.robjects = robjects
-            self.rpackages = packages
-            self.rvectors = vectors
+            cls.robjects = robjects
+            cls.rpackages = packages
+            cls.vectors = vectors
 
             numpy2ri.activate()
         except ImportError:
-            raise ImportError(f"Expects installed python package 'rpy2', could not find it. "
-                              f"Did you install ordcomp with the r_wrapper extras? "
-                              f"pip install ordcomp[r_wrapper]")
+            raise ImportError("Expects installed python package 'rpy2', could not find it. "
+                              "Did you install ordcomp with the r_wrapper extras? "
+                              "pip install ordcomp[r_wrapper]")
 
+    @classmethod
+    def import_r_package(cls, package_name, install_if_missing=True, **kwargs):
+        if not hasattr(RWrapperMixin, 'robjects'):
+            cls._init_r()
 
-    def import_r_package(self, package_name, install_if_missing=True, **kwargs):
-        if not hasattr(self, 'robjects'):
-            self._init_r()
-
-        if package_name not in RWrapper.imported_packages:
-            if not self.rpackages.isinstalled(package_name):
+        if package_name not in RWrapperMixin.imported_packages:
+            if not cls.rpackages.isinstalled(package_name):
                 if install_if_missing:
-                    utils = self.rpackages.importr('utils')
+                    utils = cls.rpackages.importr('utils')
                     utils.chooseCRANmirror(ind=1)
-                    utils.install_packages(self.rvectors.StrVector([package_name]))
+                    utils.install_packages(cls.rvectors.StrVector([package_name]))
                 else:
                     raise ImportError(f"Expects installed R package '{package_name}', could not find it.")
 
-            RWrapper.imported_packages[package_name] = self.rpackages.importr(package_name, **kwargs)
+            RWrapperMixin.imported_packages[package_name] = cls.rpackages.importr(package_name, **kwargs)
 
-        setattr(self, package_name, RWrapper.imported_packages[package_name])
+        setattr(cls, package_name, RWrapperMixin.imported_packages[package_name])
 
-    def seed_r(self, random_state):
-        base = self.rpackages.importr('base')
-        #base.set_seed(random_state.randint(-1e9, 1e9))
-
-
+    @classmethod
+    def seed_r(cls, random_state):
+        base = cls.rpackages.importr('base')
+        base.set_seed(random_state.randint(-1e9, 1e9))
