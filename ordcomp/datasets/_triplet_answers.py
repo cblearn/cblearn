74e4cb1404196381caef00c0b4386de44f76d282
David-Elias Kuenstle <david-elias.kuenstle@uni-tuebingen.de>
2020-12-28 16:35:13 +0100

    Allow precomputed distances to generate triplet answers

--- ordcomp/datasets/_triplet_answers.py
+++ ordcomp/datasets/_triplet_answers.py
@@ -14,18 +14,24 @@ class NoiseTarget(enum.Enum):
     DIFFERENCES = 'differences'
 
 
+class Distance(enum.Enum):
+    EUCLIDEAN = 'euclidean'
+    PRECOMPUTED = 'precomputed'
+
+
 def noisy_triplet_answers(triplets: utils.Triplets, embedding: np.ndarray,
                           question_format: Optional[str] = None,
                           answer_format: Optional[str] = None,
                           noise: Union[None, str, Callable] = None,
-                          noise_options: Dict = {}, noise_target: str = 'points',
-                          random_state: Union[None, int, np.random.RandomState] = None
+                          noise_options: Dict = {}, noise_target: Union[str, NoiseTarget] = 'differences',
+                          random_state: Union[None, int, np.random.RandomState] = None,
+                          distance: Union[str, Distance] = 'euclidean'
                           ) -> utils.TripletAnswers:
     """ Triplet answers for an embedding with noise.
 
     Args:
         triplets: Numpy array or sparse matrix of triplet indices
-        embedding: Numpy array of object coordinates, (n_objects, n_components)
+        embedding: Numpy array of object coordinates, (n_objects, n_components) or distance matrix (n_objects, n_objects)
         question_format: Format of the triplet questions. If none, keeps input format.
         answer_format: Triplet format of answers.
         noise: Noise distribution.
@@ -36,6 +42,8 @@ def noisy_triplet_answers(triplets: utils.Triplets, embedding: np.ndarray,
         noise_target: 'points' if noise should be added to triplet coordinates or
                       'differences' if noise should be added to distance difference.
         random_state: State or seed for noise sampling.
+        distance: {'euclidean', 'precomputed'}. Specifies distance metrix between embedding points
+                  or if distances are passed directly as distance matrix.
     Returns:
         Answers in format as defined by answer_format,
         either numpy array (n_triplets,) or sparse matrix
@@ -43,26 +51,32 @@ def noisy_triplet_answers(triplets: utils.Triplets, embedding: np.ndarray,
         If return_indices is True, a tuple of indices and answers can be returned
     """
     noise_target = NoiseTarget(noise_target)
+    distance = Distance(distance)
     input_question_format, input_answer_format = utils.triplet_format(triplets)
     question_format = utils.QuestionFormat(question_format or input_question_format)
     answer_format = utils.AnswerFormat(answer_format or input_answer_format)
 
     triplets: np.ndarray = utils.check_triplet_questions(triplets, format=utils.QuestionFormat.LIST)
     embedding = check_array(embedding)
-    input_dim = embedding.shape[1]
-
-    y_triplets = embedding[triplets.ravel()].reshape(-1, 3 * input_dim)
     if isinstance(noise, str):
         random_state = check_random_state(random_state)
         noise_fun: Callable = getattr(random_state, noise)
     elif callable(noise):
         noise_fun = noise
-    if noise is not None and noise_target is NoiseTarget.POINTS:
-        y_triplets += noise_fun(size=y_triplets.shape, **noise_options)
 
-    pivot = y_triplets[:, 0:input_dim]
-    differences = (pairwise.paired_euclidean_distances(pivot, y_triplets[:, input_dim:(2 * input_dim)])
-                   - pairwise.paired_euclidean_distances(pivot, y_triplets[:, (2 * input_dim):]))
+    if distance is Distance.EUCLIDEAN:
+        y_triplets = embedding[triplets.ravel()].reshape(-1, 3, embedding.shape[1])
+        if noise is not None and noise_target is NoiseTarget.POINTS:
+            y_triplets += noise_fun(size=y_triplets.shape, **noise_options)
+        near_distance = pairwise.paired_euclidean_distances(y_triplets[:, 0], y_triplets[:, 1])
+        far_distance = pairwise.paired_euclidean_distances(y_triplets[:, 0], y_triplets[:, 2])
+    elif distance is Distance.PRECOMPUTED:
+        if noise is not None and noise_target is NoiseTarget.POINTS:
+            raise ValueError("Applying noise on points is not possible for precomputed distances.")
+        near_distance = embedding[triplets[:, 0], triplets[:, 1]]
+        far_distance = embedding[triplets[:, 0], triplets[:, 2]]
+
+    differences = near_distance - far_distance
     if noise is not None and noise_target is NoiseTarget.DIFFERENCES:
         differences += noise_fun(size=differences.shape, **noise_options)
 
@@ -71,10 +85,23 @@ def noisy_triplet_answers(triplets: utils.Triplets, embedding: np.ndarray,
 
 
 def triplet_answers(triplets: utils.Triplets, embedding: np.ndarray,
-                    question_format: Optional[str] = None, answer_format: Optional[str] = None) -> utils.TripletAnswers:
+                    question_format: Optional[str] = None, answer_format: Optional[str] = None,
+                    distance: Union[str, Distance] = 'euclidean') -> utils.TripletAnswers:
     """ Triplet answers for an embedding.
 
-    >>> triplets, answers = triplet_answers([[1, 0, 2], [1, 2, 0]], [[0], [4], [5]], answer_format='boolean')
+    The default assumes Euclidean distances between embedding points.
+
+    >>> triplets = [[1, 0, 2], [1, 2, 0]]
+    >>> points = [[0], [4], [5]]
+    >>> triplets, answers = triplet_answers(triplets, points, answer_format='boolean')
+    >>> answers
+    array([False,  True])
+
+    To use alternative distance metrics, you can pass precomputed distances instead of an embedding.
+
+    >>> from sklearn.metrics import pairwise
+    >>> distances = pairwise.manhattan_distances(points)
+    >>> triplets, answers = triplet_answers(triplets, distances, answer_format='boolean', distance='precomputed')
     >>> answers
     array([False,  True])
 
@@ -83,7 +110,8 @@ def triplet_answers(triplets: utils.Triplets, embedding: np.ndarray,
         embedding: Numpy array of object coordinates, (n_objects, n_components)
         question_format: Format of the triplet questions. If none, keeps input format.
         answer_format: Triplet format of answers. If none, keeps input format.
-        return_indices: Return triplet indices along with answers
+        distance: {'euclidean', 'precomputed'}. Specifies distance metrix between embedding points
+                  or if distances are passed directly as distance matrix.
     Returns:
         Answers in format as defined by answer_format
         either numpy array (n_triplets,) or sparse matrix
@@ -91,4 +119,4 @@ def triplet_answers(triplets: utils.Triplets, embedding: np.ndarray,
         If return_indices is True, a tuple of indices and answers can be returned
     """
     return noisy_triplet_answers(triplets, embedding, noise=None,
-                                 question_format=question_format, answer_format=answer_format)
+                                 question_format=question_format, answer_format=answer_format, distance=distance)
--- ordcomp/datasets/_triplet_simulation.py
+++ ordcomp/datasets/_triplet_simulation.py
@@ -5,7 +5,7 @@
 """
 import numpy as np
 
-from typing import Union, Callable, Dict
+from typing import Union
 
 from ._triplet_indices import make_all_triplet_indices
 from ._triplet_indices import make_random_triplet_indices
@@ -14,6 +14,18 @@ from .. import utils
 
 
 def make_all_triplets(embedding: np.ndarray, question_format, answer_format, monotonic: bool = False, **kwargs):
+    """ Make all possible triplets with answers for the provided embedding or distances.
+
+        Args:
+            embedding: Object coordinates or distance matrix
+            question_format: Result triplet format
+            answer_format: Result answer format.
+            monotonic: Only triplets (j, i, k), such that j < i < k.
+            random_state: Seed for noisy answers
+            kwargs: Additional arguments passed to :func:`ordcomp.datasets.noisy_triplet_answers`
+        Returns:
+            The triplets and answers, based on format. See :func:`ordcomp.utils.check_triplets`.
+    """
     triplets = make_all_triplet_indices(len(embedding), monotonic)
     return noisy_triplet_answers(triplets, embedding, question_format=question_format, answer_format=answer_format,
                                  **kwargs)
@@ -21,10 +33,10 @@ def make_all_triplets(embedding: np.ndarray, question_format, answer_format, mon
 
 def make_random_triplets(embedding: np.ndarray, size: Union[int, float] = 1.,
                          question_format='list', answer_format='boolean',
-                         noise: Union[None, str, Callable] = None, noise_options: Dict = {},
-                         noise_target: str = 'points', random_state: Union[None, int, np.random.RandomState] = None,
-                         repeat: bool = True, monotonic: bool = False, make_all: int = 10000) -> utils.Triplets:
-    """ Make triplets with answers.
+                         random_state: Union[None, int, np.random.RandomState] = None,
+                         repeat: bool = True, monotonic: bool = False, make_all: int = 10000, **kwargs
+                         ) -> utils.Triplets:
+    """ Make random triplets with answers for the provided embedding or distances.
 
         >>> triplets, answers = make_random_triplets(np.random.rand(12, 2),size=1000)
         >>> answers.shape, np.unique(answers).tolist()
@@ -33,25 +45,19 @@ def make_random_triplets(embedding: np.ndarray, size: Union[int, float] = 1.,
         ((1000, 3), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
 
         Args:
-            embedding: Object coordinates as numpy array, (n_objects, n_components).
+            embedding: Object coordinates (n_objects, n_components) or distance matrix (n_objects, n_objects).
             size: Either absolute or relative number of triplets to generate.
+            question_format: Result triplet format
             answer_format: Result answer format.
-            noise: Noise distribution for answers.
-                   Can be the name of a distribution function from :class:`numpy.random.RandomState`
-                   or a function accepting the same arguments.
-                   If None, no noise will be applied.
-            noise_options: Additional arguments passed to the noise function as keyword arguments.
-            noise_target: 'points' if noise should be added to triplet coordinates or
-                          'differences' if noise should be added to distance difference.
-            random_state: State or seed for triplet index and noise sampling.
             repeat: Sample triplet indices with repetitions
             monotonic: Sample triplets (j, i, k), such that j < i < k.
             make_all: Choose from all triplets instead of iterative sampling,
                       if the difference between all triplets to the requested number is smaller than this value.
+            random_state: Seed for triplet sampling and noisy answers
+            kwargs: Additional arguments passed to :func:`ordcomp.datasets.noisy_triplet_answers`
         Returns:
             The triplets and answers, based on format. See :func:`ordcomp.utils.check_triplets`.
     """
     triplets = make_random_triplet_indices(len(embedding), size, random_state, repeat, monotonic, make_all)
     return noisy_triplet_answers(triplets, embedding, question_format=question_format, answer_format=answer_format,
-                                 noise=noise, noise_options=noise_options,
-                                 noise_target=noise_target, random_state=random_state)
+                                 random_state=random_state, **kwargs)
