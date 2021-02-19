8555235758abbdcad9646e27ef8cd8ff70ff6d6c
Leena C Vankadara <vleena@amazon.com>
2021-02-19 13:25:06 +0100

    Initial commit - Adding GNMDS X version

--- cblearn/embedding/_gnmds.py
+++ cblearn/embedding/_gnmds.py
@@ -12,9 +12,9 @@ from cblearn.utils import assert_torch_is_available, torch_minimize_lbfgs
 
 
 class GNMDS(BaseEstimator, TripletEmbeddingMixin):
-    """ Generaized Non-metric Multidimensional Scaling  (GNMDS).
+    """ Generalized Non-metric Multidimensional Scaling (GNMDS).
 
-        SOE [1]_ is minimizing the soft objective as a smooth relaxation of the triplet error.
+        GNMDS [1]_ minimizes a kernel version of the triplet hinge soft objective as a smooth relaxation of the triplet error.
 
         This estimator supports multiple implementations which can be selected by the `algorithm` parameter.
         The majorizing algorithm for SOE is described in the paper original paper [1]_.
@@ -35,7 +35,7 @@ class GNMDS(BaseEstimator, TripletEmbeddingMixin):
         >>> triplets = datasets.make_random_triplets(true_embedding, result_format='list-order', size=1000)
         >>> triplets.shape, np.unique(triplets).shape
         ((1000, 3), (15,))
-        >>> estimator = SOE(n_components=2, random_state=42)
+        >>> estimator = GNMDS(n_components=2, random_state=42)
         >>> embedding = estimator.fit_transform(triplets, n_objects=15)
         >>> embedding.shape
         (15, 2)
@@ -44,7 +44,7 @@ class GNMDS(BaseEstimator, TripletEmbeddingMixin):
 
         The following is running on the CUDA GPU, if available (but requires pytorch installed).
 
-        >>> estimator = SOE(n_components=2, algorithm="backprop", random_state=42)
+        >>> estimator = GNMDS(n_components=2, algorithm="X", random_state=42)
         >>> embedding = estimator.fit_transform(triplets, n_objects=15)
         >>> estimator.score(triplets)
         1.0
@@ -57,14 +57,18 @@ class GNMDS(BaseEstimator, TripletEmbeddingMixin):
                Arxiv Preprint, https://arxiv.org/abs/1912.01666
         """
 
-    def __init__(self, n_components=2, max_iter=1000, verbose=False,
+    def __init__(self, n_components=2, margin=1, max_iter=1000, verbose=False,
                  random_state: Union[None, int, np.random.RandomState] = None,
-                 algorithm: str = "K", device: str = "auto"):
+                 algorithm: str = "X", device: str = "auto"):
         """ Initialize the estimator.
 
         Args:
             n_components :
                 The dimension of the embedding.
+            margin:
+                Scale parameter which only takes strictly positive value.
+                Defines the intended minimal difference of distances in the embedding space between
+                for any triplet.
             max_iter:
                 Maximum number of optimization iterations.
             verbose: boolean, default=False
@@ -72,12 +76,13 @@ class GNMDS(BaseEstimator, TripletEmbeddingMixin):
             random_state:
                 The seed of the pseudo random number generator used to initialize the optimization.
             algorithm:
-                The algorithm used to optimize the soft objective. {"K", "X"}
+                The algorithm used to optimize the soft objective. {"majorizing", "backprop"}
             device: The device on which pytorch computes. {"auto", "cpu", "cuda"}
                 "auto" chooses cuda (GPU) if available, but falls back on cpu if not.
                 This parameter is only used if "backprop" algorithm is used.
         """
         self.n_components = n_components
+        self.margin = margin
         self.max_iter = max_iter
         self.verbose = verbose
         self.random_state = random_state
@@ -103,40 +108,47 @@ class GNMDS(BaseEstimator, TripletEmbeddingMixin):
             init = random_state.multivariate_normal(np.zeros(self.n_components), np.eye(self.n_components),
                                                     size=n_objects)
 
-        if self.algorithm == "X":
+        if self.algorithm == "K":
+            """
+            The kernel version of the algorithm.
+            """
             assert_torch_is_available()
-            result = torch_minimize_lbfgs(_gnmds_x_loss_torch, init, args=(triplets.astype(int)),
+            result = torch_minimize_lbfgs(_gnmds_x_loss_torch, init, args=(triplets.astype(int), self.margin),
                                           device=self.device, max_iter=self.max_iter)
-        elif self.algorithm == "K":
-            result = minimize(_gnmds_k_loss, init.ravel(), args=(init.shape, triplets),
-                              method='L-BFGS-B',
-                              options=dict(maxiter=self.max_iter, disp=self.verbose))
+        elif self.algorithm == "X":
+            """
+                The embedding (X) version of the algorithm.
+            """
+            assert_torch_is_available()
+            result = torch_minimize_lbfgs(_gnmds_x_loss_torch, init, args=(triplets.astype(int), self.margin),
+                                          device=self.device, max_iter=self.max_iter)
         else:
-            raise ValueError(f"Unknown GNMDS algorithm '{self.algorithm}'. Try 'K' or 'X'.")
+            raise ValueError(f"Unknown SOE algorithm '{self.algorithm}'. Try 'K' or 'X' instead.")
 
         if self.verbose and not result.success:
-            print(f"GNMDS's optimization failed with reason: {result.message}.")
+            print(f"SOE's optimization failed with reason: {result.message}.")
         self.embedding_ = result.x.reshape(-1, self.n_components)
         self.stress_, self.n_iter_ = result.fun, result.nit
         return self
 
 
 def _gnmds_x_loss_torch(embedding, triplets, margin):
-    """ Loss using the data matrix """
+    """ Equation (1) of Terada & Luxburg (2014) """
     import torch  # Pytorch is an optional dependency
 
     X = embedding[triplets]
     anchor, positive, negative = X[:, 0, :], X[:, 1, :], X[:, 2, :]
     triplet_loss = torch.nn.functional.triplet_margin_loss(anchor, positive, negative,
-                                                           p=2, reduction='none')
+                                                           margin=margin, p=2, reduction='none')
     return torch.sum(triplet_loss**2)
 
 
-def _gnmds_k_loss(x, x_shape, triplets, margin):
-    """TODO: Loss using the kernel matrix """
+def _gnmds_x_loss(x, x_shape, triplets, margin):
+    """ Equation (1) of Terada & Luxburg (2014) """
     X = x.reshape(x_shape)
     X_dist = distance_matrix(X, X)
     ij_dist = X_dist[triplets[:, 0], triplets[:, 1]]
     kl_dist = X_dist[triplets[:, 0], triplets[:, 2]]
     stress = np.maximum(ij_dist + margin - kl_dist, 0) ** 2
     return stress.sum()
+
