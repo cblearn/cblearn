975fccd6b726f0060a2c388f0f90459204a04c3f
David-Elias Künstle <dekuenstle@users.noreply.github.com>
2021-07-13 15:13:23 +0200

    Return scaler instead of torch array stress

--- cblearn/embedding/_torch_utils.py
+++ cblearn/embedding/_torch_utils.py
@@ -103,8 +103,8 @@ def torch_minimize(method,
         message = f"{method} did not converge."
 
     return scipy.optimize.OptimizeResult(
-        x=X.cpu().detach().numpy(), fun=loss, nit=n_iter,
-        success=success, message=message)
+        x=X.cpu().detach().numpy(), fun=loss.cpu().detach().numpy()[0], 
+        nit=n_iter, success=success, message=message)
 
 
 def assert_torch_is_available() -> None:
