b4ab335e6d21a806f29f66c6cc10737b17766028
David-Elias Kuenstle <david-elias.kuenstle@uni-tuebingen.de>
2022-05-15 19:51:30 +0200

    Improve soe repeated initialization test by statistical test

--- cblearn/embedding/tests/test_soe.py
+++ cblearn/embedding/tests/test_soe.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
 from scipy.optimize import check_grad
+from scipy.stats import ttest_rel
 from cblearn.datasets import make_random_triplets
 
 from cblearn.embedding._soe import SOE, _soe_loss
@@ -10,24 +11,23 @@ def test_soe_repeat():
     """ Test that the repeated initialization leads to stable results. """
     n, d = 20, 1  # optimization is most unstable for d = 1
     X = np.random.randn(n, d)
-    T = make_random_triplets(X, size=2*d*n*np.log(n), result_format='list-order',
-                             noise='normal', noise_options=dict(scale=0.1))
-    T_test = make_random_triplets(X, size=10000, result_format='list-order',
-                             noise='normal', noise_options=dict(scale=0.1))
-    accuracy_threshold = 0.8  # empirically determined
-
-    # is this an unstable condition? Then single inititalization should sometimes have low accuracy.
-    with pytest.raises(AssertionError):
-        soe_one = SOE(n_components=d, n_init=1)
-        for _ in range(3):
-            soe_one.fit(T)
-            assert soe_one.score(T_test) > accuracy_threshold
 
     # does repeated initialization lead to stable results?
-    soe_repeat = SOE(n_components=d, n_init=10)
-    for _ in range(3):
-        stress = soe_repeat.fit(T).stress_
-        assert soe_repeat.score(T_test) > accuracy_threshold
+    score_one = []
+    score_repeat = []
+    for i in range(10):
+        T = make_random_triplets(X, size=2 * d * n * np.log(n), result_format='list-order',
+                                 noise='normal', noise_options=dict(scale=0.1))
+        T_test = make_random_triplets(X, size=10000, result_format='list-order',
+                                      noise='normal', noise_options=dict(scale=0.1))
+        soe_one = SOE(n_components=d, n_init=1)
+        soe_repeat = SOE(n_components=d, n_init=10)
+        score_one.append(soe_one.fit(T).score(T_test))
+        score_repeat.append(soe_repeat.fit(T).score(T_test))
+
+    stat = ttest_rel(score_one, score_repeat, alternative='less')
+    assert stat.pvalue < 0.05
+
 
 
 @pytest.mark.parametrize('n,d', [(20, 1), (50, 2), (100, 3)])
